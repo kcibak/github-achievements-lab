aac45d8ef55b8ac61eb4bc471fd93a7c145407ef
kcibak <cibakkn@gmail.com>
2026-02-24 19:24:46 -0500

    Add levelup.c with GitHub achievements list and practice instructions

---
+++ levelup.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main() {
+    printf("GitHub Achievements Lab - Level Up!\n\n");
+    printf("Here are some GitHub achievements and how to unlock them:\n\n");
+
+    printf("1. Hello World - Create your first repository\n");
+    printf("2. YOLO - Merge a pull request without a review\n");
+    printf("3. Quickdraw - Close an issue within 5 minutes of opening it\n");
+    printf("4. Pair Extraordinaire - Co-author a commit\n");
+    printf("5. Pull Shark - Open 2 pull requests that get merged\n");
+    printf("6. Galaxy Brain - Have 2 pull requests merged\n");
+    printf("7. Starstruck - Have 16 public repositories starred by others\n");
+    printf("8. Arctic Code Vault Contributor - Contribute to a repository in the Arctic Code Vault\n");
+    printf("9. Public Sponsor - Sponsor an open source contributor\n");
+    printf("10. GitHub Sponsor - Become a GitHub Sponsor\n");
+    printf("11. Heart On Your Sleeve - Have a sponsor button in your repository\n");
+    printf("12. Open Sourcerer - Have 1 or more repositories with 1 or more stars\n");
+    printf("13. Forker - Have 1 or more repositories with 1 or more forks\n");
+    printf("14. 2020 Game on - Contribute to a repository during Hacktoberfest 2020\n");
+    printf("15. 2021 Game on - Contribute to a repository during Hacktoberfest 2021\n");
+    printf("16. 2022 Game on - Contribute to a repository during Hacktoberfest 2022\n");
+    printf("17. 2023 Game on - Contribute to a repository during Hacktoberfest 2023\n");
+    printf("18. 2024 Game on - Contribute to a repository during Hacktoberfest 2024\n");
+    printf("19. 2025 Game on - Contribute to a repository during Hacktoberfest 2025\n");
+    printf("20. My First PR - Create your first pull request\n");
+    printf("21. My First Issue - Create your first issue\n");
+    printf("22. My First Code Review - Review a pull request\n");
+    printf("23. My First Discussion - Create your first discussion\n");
+    printf("24. My First Repository - Create your first repository\n");
+    printf("25. My First Commit - Make your first commit\n");
+    printf("26. My First Branch - Create your first branch\n");
+    printf("27. My First Star - Star your first repository\n");
+    printf("28. My First Follow - Follow your first user\n");
+    printf("29. My First Gist - Create your first gist\n");
+    printf("30. My First Wiki - Create your first wiki page\n");
+    printf("31. My First Release - Create your first release\n");
+    printf("32. My First Package - Publish your first package\n");
+    printf("33. My First Project - Create your first project board\n");
+    printf("34. My First Security Advisory - Publish your first security advisory\n");
+    printf("35. My First Sponsorship - Sponsor someone\n");
+    printf("36. My First Sponsor - Get sponsored\n");
+    printf("37. My First Contribution - Contribute to a repository\n");
+    printf("38. My First Merge - Merge a pull request\n");
+    printf("39. My First Review - Review a pull request\n");
+    printf("40. My First Comment - Comment on an issue or pull request\n");
+    printf("41. My First Reaction - React to an issue or pull request\n");
+    printf("42. My First Mention - Get mentioned in an issue or pull request\n");
+    printf("43. My First Assignment - Get assigned to an issue or pull request\n");
+    printf("44. My First Label - Add a label to an issue or pull request\n");
+    printf("45. My First Milestone - Create a milestone\n");
+    printf("46. My First Lock - Lock a conversation\n");
+    printf("47. My First Pin - Pin an issue\n");
+    printf("48. My First Transfer - Transfer an issue\n");
+    printf("49. My First Archive - Archive a repository\n");
+    printf("50. My First Template - Use an issue template\n");
+
+    printf("\nUse this dummy repo to practice and unlock these achievements!\n");
+    printf("Level up!\n");
+
+    return 0;
+}
